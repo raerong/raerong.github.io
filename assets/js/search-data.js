3d11be884b4e71a5d9f8acfc6b2b6bfce114530f
raerong <163561783+raerong@users.noreply.github.com>
2026-05-11 18:32:23 +0000

    Deploying to gh-pages from @ raerong/raerong.github.io@5c8c9c21ca573e9e220d82f07e1505e239a2ccdd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
